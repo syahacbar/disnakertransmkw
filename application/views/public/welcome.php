03b67d4509d49c73cc4a6cad750131398bef3ef9
syahacbar <syahacbar@gmail.com>
2022-07-09 01:00:08 +0900

    Update welcome.php

--- application/views/public/welcome.php
+++ application/views/public/welcome.php
@@ -56,8 +56,13 @@
               <li><a href="#">Struktur Organisasi</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#services">Layanan</a></li>
-          <li><a class="nav-link   scrollto" href="#portfolio">Informasi</a></li>
+          <li class="dropdown"><a href="#"><span>Informasi</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Berita</a></li>
+              <li><a href="#">Pengumuman</a></li>
+              <li><a href="#">Pelatihan</a></li>
+            </ul>
+          </li>
           <li class="dropdown"><a href="#"><span>Layanan</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Kartu Kuning (AK-1)</a></li>
@@ -78,11 +83,11 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-          <h1>Better Solutions For Your Business</h1>
-          <h2>We are team of talented designers making websites with Bootstrap</h2>
+          <h1>Selamat Datang</h1>
+          <h2> di Website Resmi Dinas Tenaga Kerja dan Transmigrasi Kabupaten Manokwari</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
-            <a href="#about" class="btn-get-started scrollto">Get Started</a>
-            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+            <a href="#about" class="btn-get-started scrollto">Buat Kartu Kuning</a>
+            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Selayang Pandang</span></a>
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
