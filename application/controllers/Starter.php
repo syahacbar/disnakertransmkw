b63eefa458fe29031d337bdca5c5da402c8bd63f
syahacbar <syahacbar@gmail.com>
2022-07-11 16:46:33 +0900

    Update Starter.php

--- application/controllers/Starter.php
+++ application/controllers/Starter.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Starter extends MY_Controller
+class Starter extends CI_Controller
 {
 
 	public function __construct()
