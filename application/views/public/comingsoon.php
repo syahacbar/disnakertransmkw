bea6d46e0825678472405fa0c0d7998471e7e60e
syahacbar <syahacbar@gmail.com>
2022-07-11 15:02:15 +0900

    favicon

--- application/views/public/comingsoon.php
+++ application/views/public/comingsoon.php
@@ -4,13 +4,15 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>ComingSoon - Bootstrap Coming Soon Template</title>
+  <title>Disnakertrans Manokwari</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="<?php echo base_url();?>/assets/frontend/comingsoon/assets/img/favicon.png" rel="icon">
-  <link href="<?php echo base_url();?>/assets/frontend/comingsoon/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url();?>/assets/frontend/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url();?>/assets/frontend/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url();?>/assets/frontend/favicon/favicon-16x16.png">
+  <link rel="manifest" href="<?php echo base_url();?>/assets/frontend/favicon/site.webmanifest">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
