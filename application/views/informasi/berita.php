a3f92e08e319fdd8fd50d884b546b7347f7e3327
syahacbar <syahacbar@gmail.com>
2022-07-13 21:43:59 +0900

    tambahan menu

---
+++ application/views/informasi/berita.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed'); ?>
+
+<?php include viewPath('includes/header'); ?>
+
+ini halaman berita
+
+<?php include viewPath('includes/footer'); ?>
