e90c683b68a103b06ed81d17bdd219028f64632c
syahacbar <syahacbar@gmail.com>
2022-07-19 22:01:09 +0900

    werwrewwer

--- application/views/web/form_registrasi.php
+++ application/views/web/form_registrasi.php
@@ -1,45 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-    <title><?php echo $page->title ?> | <?php echo $page->site_title ?> </title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
-
-    <!-- Favicons -->
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/img/favicon.png" rel="icon">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-    <!-- Vendor CSS Files -->
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-    <!-- Template Main CSS File -->
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/css/style.css" rel="stylesheet">
-
-    <style>
-        header#header {
-            background: #283a5a !important;
-        }
-
-        section#contact {
-            margin-top: 120px;
-        }
-    </style>
-</head>
-
-<body>
 
     <?php include viewPath('web/template/header'); ?>
 
