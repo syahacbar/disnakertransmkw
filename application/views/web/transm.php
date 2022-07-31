9e560e3cea02e0c327127fef66a18ffa14a8f537
syahacbar <syahacbar@gmail.com>
2022-07-31 09:02:18 +0900

    update header dan footer hal transm dan tenaker

--- application/views/web/transm.php
+++ application/views/web/transm.php
@@ -1,40 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
-<head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-    <title><?php echo $page->title ?> | <?php echo $page->site_title ?> </title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
-
-    <!-- Favicons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url(); ?>/assets/frontend/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url(); ?>/assets/frontend/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>/assets/frontend/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?php echo base_url(); ?>/assets/frontend/favicon/site.webmanifest">
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-    <!-- Vendor CSS Files -->
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-    <!-- Template Main CSS File -->
-    <link href="<?php echo base_url(); ?>/assets/frontend/assets/css/style.css" rel="stylesheet">
-
-    <!-- Dropzone CSS -->
-    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
-
-    <!-- Lightbox -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/plugins/lightbox-previewer/src/index.css" />
-    <script src="<?php echo base_url(); ?>/assets/plugins/lightbox-previewer/dist/img-previewer.min.js"></script>
+<?php include viewPath('web/template/header'); ?>
 
     <style>
         header#header {
@@ -143,46 +110,7 @@
     </style>
 </head>
 
-<body>
- 
-    <!-- ======= Header ======= -->
-    <header id="header" class="fixed-top ">
-        <div class="container d-flex align-items-center">
 
-            <h1 class="logo me-auto"><a href="<?php echo site_url(); ?>">Disnakertrans Manokwari</a></h1>
-            <!-- Uncomment below if you prefer to use an image logo -->
-            <!-- <a href="index.html" class="logo me-auto"><img src="<?php echo base_url(); ?>/assets/frontend/assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-            <nav id="navbar" class="navbar">
-                <ul>
-                    <li><a class="nav-link scrollto <?php echo ($page->menu == 'beranda') ? 'active' : '' ?>" href="<?php echo site_url(); ?>">Beranda</a></li>
-                    <li class="dropdown"><a class="<?php echo ($page->menu == 'profil') ? 'active' : '' ?>" href="<?php echo site_url(); ?>web/profil"><span>Profil</span></a></li>
-                    <li class="dropdown"><a class="<?php echo ($page->menu == 'bidang') ? 'active' : '' ?>" href="#"><span>Bidang-Bidang</span> <i class="bi bi-chevron-down"></i></a>
-                        <ul>
-                            <li><a href="<?php echo site_url(); ?>web/transmigrasi">Bidang Transmigrasi</a></li>
-                            <li><a href="<?php echo site_url(); ?>web/tenagakerja">Bidang Tenaga Kerja</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown"><a class="<?php echo ($page->menu == 'informasi') ? 'active' : '' ?>" href="#"><span>Informasi</span> <i class="bi bi-chevron-down"></i></a>
-                        <ul>
-                            <li><a href="<?php echo site_url(); ?>web/berita">Berita</a></li>
-                            <li><a href="<?php echo site_url(); ?>web/pengumuman">Pengumuman</a></li>
-                            <li><a href="<?php echo site_url(); ?>web/pelatihan">Pelatihan</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown"><a class="<?php echo ($page->menu == 'layanan') ? 'active' : '' ?>" href="#"><span>Layanan</span> <i class="bi bi-chevron-down"></i></a>
-                        <ul>
-                            <li><a href="<?php echo site_url(); ?>web/registrasi">Kartu Pencari Kerja (Kartu Kuning)</a></li>
-                        </ul>
-                    </li>
-                    <li><a class="nav-link scrollto <?php echo ($page->menu == 'kontak') ? 'active' : '' ?>" href="#">Kontak</a></li>
-                    <li><a target="_blank" class="getstarted scrollto" href="<?php echo site_url('dashboard'); ?>">Masuk</a></li>
-                </ul>
-                <i class="bi bi-list mobile-nav-toggle"></i>
-            </nav><!-- .navbar -->
-
-        </div>
-    </header><!-- End Header -->
 
     <main id="main">
         <section id="visimisi" class="about mt-10">
@@ -712,72 +640,10 @@
 
     </main><!-- End #main -->
 
-    <!-- ======= Footer ======= -->
-    <!-- ======= Footer ======= -->
-    <footer id="footer">
+   <!-- ======= Footer ======= -->
+<?php include viewPath('web/template/footer'); ?>
+<!-- End Footer -->
 
-        <div class="footer-top">
-            <div class="container">
-                <div class="row">
-
-                    <div class="col-lg-3 col-md-6 footer-contact">
-                        <h3>Disnakertrans Manokwari</h3>
-                        <p>
-                            Jl. Percetakan Negara<br>
-                            Kel. Sanggeng, Kec. Manokwari Barat<br>
-                            Kab. Manokwari, Prov. Papua Barat <br><br>
-                            <strong>Telp:</strong> 0986-211934, 0986-211738<br>
-                            <strong>Email:</strong> info@disnakertransmkw.com<br>
-                        </p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 footer-links">
-                        <h4>Informasi</h4>
-                        <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Berita</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Pengumuman</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Pelatihan</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 footer-links">
-                        <h4>Link Terkait</h4>
-                        <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a target="_blank" href="https://kemnaker.go.id/">Kemenaker</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a target="_blank" href="https://papuabaratprov.go.id/">Provinsi Papua Barat</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a target="_blank" href="https://disnakertrans.papuabaratprov.go.id/">Disnakertrans Papua Barat</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a target="_blank" href="https://www.manokwarikab.go.id">Pemkab. Manokwari</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 footer-links">
-                        <h4>Lebih Dekat Dengan Kami</h4>
-                        <p>Tetap dekat dan mendapat informasi-informasi terbaru dari kami melalui media sosial kami</p>
-                        <div class="social-links mt-3">
-                            <a target="_blank" href="https://facebook.com" class="facebook"><i class="bx bxl-facebook"></i></a>
-                            <a target="_blank" href="https://twitter.com" class="twitter"><i class="bx bxl-twitter"></i></a>
-                            <a target="_blank" href="https://instagram.com" class="instagram"><i class="bx bxl-instagram"></i></a>
-                            <a target="_blank" href="https://youtube.com" class="youtube"><i class="bx bxl-youtube"></i></a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-        <div class="container footer-bottom clearfix">
-            <div class="copyright">
-                Copyright &copy; 2022 <strong><span>Disnakertrans Manokwari</span></strong>
-            </div>
-            <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
-                Designed by <a href="https://manokwariweb.com/">ManokwariWeb</a>
-            </div>
-        </div>
-    </footer><!-- End Footer -->
 
     <!-- Dropzone JS -->
     <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
